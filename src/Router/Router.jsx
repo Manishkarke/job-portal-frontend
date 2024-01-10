8ff3b4594693bf83f004c172bf023976e2f7d977
Manishkarke <mk4345437@gmail.com>
2024-01-10 16:45:05 +0545

    Changed the path of the imports

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { getDataFromLocalStorage } from "../utils/localStorage";
-import Dashboard from "../components/Dashboard/Dashboard";
-import Login from "../components/Login/Login";
-import Register from "../components/Register/Register";
+import Dashboard from "../Pages/Dashboard/Dashboard";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 export default function Router() {
   return (
